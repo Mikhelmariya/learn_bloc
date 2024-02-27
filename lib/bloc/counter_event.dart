913b730af8bd3e5c0994c8e497d6cf922c254cfa
Mikhel V Kuttickal <81845280+Mikhelmariya@users.noreply.github.com>
2024-02-27 20:09:14 +0530

    Incrementing counter

--- lib/bloc/counter_event.dart
+++ lib/bloc/counter_event.dart
@@ -1,10 +1,4 @@
-// when there are multiple events ,use sealed
-sealed class CounterEvent{
 
-}
-class CounterIncrementEvent extends CounterEvent{
-
-}
-class CounterDecrementEvent extends CounterEvent{
-
+class CounterIncrementEvent{
+  
 }
